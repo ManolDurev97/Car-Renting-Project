e93caa9d89d625b75932a67224501bf08cdb9a2b
ManolDurev97 <ch00s3r@abv.bg>
2023-08-02 17:41:26 +0300

    Cleaned the project

---
+++ CarRenting/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿namespace CarRenting.Controllers
+{
+    using CarRenting.Models;
+    using Microsoft.AspNetCore.Mvc;
+    using System.Diagnostics;
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger) => _logger = logger;
+        
+
+        public IActionResult Index() => View();
+        
+
+        public IActionResult Privacy() => View();
+        
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        
+    }
+}
