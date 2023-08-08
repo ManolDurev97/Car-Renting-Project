a9f48788abcda6e0770a6abf729a2f2df48ebf9f
ManolDurev97 <ch00s3r@abv.bg>
2023-08-08 15:44:50 +0300

    create AddFormModel and Add view

--- CarRenting/Controllers/CarController.cs
+++ CarRenting/Controllers/CarController.cs
@@ -7,9 +7,20 @@ namespace CarRenting.Controllers
     {
         public IActionResult Add() => View();
 
-        public IActionResult Add(AddCarFormModel model)
+        [HttpPost]
+        public IActionResult Add(AddCarFormModel car)
         {
-            return View(model);
+            var currCar = new AddCarFormModel()
+            {
+                Id = car.Id,
+                Brand = car.Brand,
+                Model = car.Model,
+                Description = car.Description,
+                ImageUrl = car.ImageUrl,
+                Year = car.Year,
+                CategoryId = car.CategoryId
+            };
+            return View(car);
         }
     }
 }
--- CarRenting/Models/Car/AddCarFormModel.cs
+++ CarRenting/Models/Car/AddCarFormModel.cs
@@ -1,6 +1,24 @@
-﻿namespace CarRenting.Models.Car
+﻿using CarRenting.Data.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarRenting.Models.Car
 {
     public class AddCarFormModel
     {
+        public int Id { get; set; }
+
+        public string Brand { get; set; }
+
+        public string Model { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string Description { get; set; }
+
+        public int Year { get; set; }
+
+        public int CategoryId { get; set; }
+
+        //public IEnumerable<Category> Categories { get; set; }
     }
 }
