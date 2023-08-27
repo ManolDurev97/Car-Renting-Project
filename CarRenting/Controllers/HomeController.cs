5ededa7a0b582da0be8c3fbfc00f4ed1798c8b51
ManolDurev97 <ch00s3r@abv.bg>
2023-08-27 15:18:30 +0300

    Аdding a Carousel to the home page and placing a link that leads to the selected car

--- CarRenting/Controllers/HomeController.cs
+++ CarRenting/Controllers/HomeController.cs
@@ -1,16 +1,40 @@
 ﻿namespace CarRenting.Controllers
 {
+    using CarRenting.Data;
     using CarRenting.Models;
+    using CarRenting.Models.Car;
     using Microsoft.AspNetCore.Mvc;
     using System.Diagnostics;
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger) => _logger = logger;
-        
+        private readonly CarRentingDbContext data;
 
-        public IActionResult Index() => View();
+        public HomeController(CarRentingDbContext data , ILogger<HomeController> logger)
+        {
+            this.data = data;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+
+            var cars = data.Cars
+                .OrderByDescending(cars => cars.Id)
+                .Select(c => new CarListingViweModel()
+                {
+                    Id = c.Id,
+                    Model = c.Model,
+                    Brand = c.Brand,
+                    ImageUrl = c.ImageUrl,
+                    Year = c.Year,
+                    Category = c.Category.Name
+                })
+                .Take(3)
+                .ToList();
+            return View(cars);
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error() => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
